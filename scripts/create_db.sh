ed3fe266d4cbfbb05db21b97afa3bb18b90899ab
kulhos <jiri.kulhan@gmail.com>
2016-01-19 01:09:29 +0000

    gde fix for GT.M 5.x

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -2,13 +2,13 @@
 ENV=${1}
 
 . ${ENV}/gtmenv > /dev/null
-[ -f ${gtmgbldir} ] && rm -f ${gtmgbldir}
+[ -f ${gtmgbldir} ] && rm -vf ${gtmgbldir}
 
 $gtm_dist/mumps -r ^GDE <<-EOF
 @${ENV}/scripts/pip.gde
 exit
 EOF
 
-[ -f ${PIP_DB} ] && rm -f ${PIP_DB}
+[ -f ${PIP_DB} ] && rm -vf ${PIP_DB}
 $gtm_dist/mupip create
 $gtm_dist/mupip load -for=zwr /pip/gbls/pip02.zwr
