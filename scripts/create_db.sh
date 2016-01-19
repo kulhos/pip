b62ab030d45f4c88a041f13b04606cb7ddc1d9d0
kulhos <jiri.kulhan@gmail.com>
2016-01-19 00:39:27 +0000

    db create and load

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -3,10 +3,12 @@ ENV=${1}
 
 . ${ENV}/gtmenv > /dev/null
 [ -f ${gtmgbldir} ] && rm -f ${gtmgbldir}
+
 $gtm_dist/mumps -r ^GDE <<-EOF
 @${ENV}/scripts/pip.gde
 exit
 EOF
 
+[ -f ${PIP_DB} ] && rm -f ${PIP_DB}
 $gtm_dist/mupip create
 $gtm_dist/mupip load -for=zwr /pip/gbls/pip02.zwr
